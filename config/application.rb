b0288e6d12b89d8a46871d9196b31361ea074caf
Joe Shoulak <joepshoulak2@gmail.com>
2014-01-24 23:41:08 -0600

    Debug

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ end
 
 module Prestongunderson
   class Application < Rails::Application
-    config.middleware.use Rack::Pjax
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
     config.active_support.escape_html_entities_in_json = true
