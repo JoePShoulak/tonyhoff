4a2f1fa72525d917c97fe6e590e25a91a88281ec
Joe Shoulak <joepshoulak2@gmail.com>
2014-04-16 08:53:40 -0500

    Update pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,19 +2,13 @@ class StaticPagesController < ApplicationController
   def home
   end
   
-  def brands
-  end
-  
   def contact
-  end
-  
-  def faq
   end
   
   def gallery
   end
   
-  def pricing
+  def pricing_artwork
   end
   
   def services
